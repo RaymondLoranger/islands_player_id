98e634642eb8ad7af4e6f189aaceee68a1412127
RaymondLoranger <Raymond.Loranger@bell.net>
2019-04-23 12:30:45 -0400

    0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.PlayerId.MixProject do
   def project do
     [
       app: :islands_player_id,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       name: "Islands Player ID",
