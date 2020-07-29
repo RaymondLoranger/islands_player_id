437a2f43d5776599b3d8ead61d469ac545146116
RaymondLoranger <Raymond.Loranger@bell.net>
2020-07-28 21:05:31 -0400

    0.1.4

--- lib/islands/player_id.ex
+++ lib/islands/player_id.ex
@@ -2,13 +2,9 @@
 # │ Inspired by the book "Functional Web Development" by Lance Halvorsen. │
 # └───────────────────────────────────────────────────────────────────────┘
 defmodule Islands.PlayerID do
-  use PersistConfig
-
-  @book_ref Application.get_env(@app, :book_ref)
-
   @moduledoc """
   Defines the `player ID` type for the _Game of Islands_.
-  \n##### #{@book_ref}
+  \n##### #{Islands.Config.get(:book_ref)}
   """
 
   @type t :: :player1 | :player2
