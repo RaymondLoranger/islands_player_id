70d99598a5f9cd6cb0e86977fdc774bd7f2eacd3
RaymondLoranger <Raymond.Loranger@bell.net>
2020-11-09 08:33:33 -0500

    0.1.8

--- lib/islands/player_id.ex
+++ lib/islands/player_id.ex
@@ -5,7 +5,7 @@ defmodule Islands.PlayerID do
   @moduledoc """
   Defines the `player ID` type for the _Game of Islands_.
 
-  ##### Based on the book [Functional Web Development](https://pragprog.com/book/lhelph/functional-web-development-with-elixir-otp-and-phoenix) by Lance Halvorsen.
+  ##### Inspired by the book [Functional Web Development](https://pragprog.com/book/lhelph/functional-web-development-with-elixir-otp-and-phoenix) by Lance Halvorsen.
   """
 
   @type t :: :player1 | :player2
