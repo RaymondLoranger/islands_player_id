4794979a526be714584ba7c54db0dc2b1de6a026
RaymondLoranger <Raymond.Loranger@bell.net>
2021-01-20 14:08:31 -0500

    0.1.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.PlayerId.MixProject do
   def project do
     [
       app: :islands_player_id,
-      version: "0.1.9",
+      version: "0.1.10",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "Islands Player ID",
@@ -45,9 +45,7 @@ defmodule Islands.PlayerId.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
-      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
-      {:mix_tasks,
-       github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 end
