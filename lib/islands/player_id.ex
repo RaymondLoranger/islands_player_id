bec46bc755dcefec1bc5b465f6e8d1e5180b64f7
RaymondLoranger <Raymond.Loranger@bell.net>
2021-12-23 17:21:33 -0500

    0.1.14

--- lib/islands/player_id.ex
+++ lib/islands/player_id.ex
@@ -2,7 +2,7 @@
 # │ Inspired by the book "Functional Web Development" by Lance Halvorsen. │
 # └───────────────────────────────────────────────────────────────────────┘
 defmodule Islands.PlayerID do
-  @player_id "[`player ID`](`t:Islands.PlayerID.t/0`)"
+  @player_id "[`player ID`](`t:t/0`)"
   @readme "https://github.com/RaymondLoranger/islands_vue_client#readme"
 
   @moduledoc """
@@ -11,6 +11,6 @@ defmodule Islands.PlayerID do
   ##### Inspired by the book [Functional Web Development](https://pragprog.com/book/lhelph/functional-web-development-with-elixir-otp-and-phoenix) by Lance Halvorsen.
   """
 
-  @typedoc "Player IDs for the Game of Islands"
+  @typedoc "Player ID for the Game of Islands"
   @type t :: :player1 | :player2
 end
